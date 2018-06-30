3207b1990fec9be2d3d6e7c1e851c3a838838f0b
Giorgi Matakheria <giomatax@gmail.com>
2018-06-30 14:27:44 +0400

    Update Read_Potentiometer_voltage_with_Arduino.ino

--- Read_Potentiometer_voltage_with_Arduino.ino
+++ Read_Potentiometer_voltage_with_Arduino.ino
@@ -1,6 +1,9 @@
+int motorPin = 9; // ძრავის + ის ფეხი 
+int motorValue = 0; //თავიდან არის 0 რომ იყოს გაჩერებული
 void setup()
 
 {
+   
 Serial.begin(9600);
 }
 
@@ -11,15 +14,20 @@ void loop(){
   int sensorValue = analogRead(A0); //პოტენციომეტრის შუა ფეხი უერთდება a0 პინს არ შეგეშალოთ თორე შეიძება დაიწვას პოტენციომეტრი 
   // ასევე გაითვალისწინეთ რომ მიღებული შედეგი უნდა გარდაქმნათ რადგან ის მოდის 0 დან 1023 ჩათვლით შესაბამისად თქვენ მიღებული შედეგი უნდა გაამრავლოთ 5 ზე და გაყოთ 1023 ზე რომ მიიღოთ შედეგი  (0 - 5V) 
   float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
+
   Serial.print("Voltage =");//ბეჭდავს შედეგს  "Voltage ="
   Serial.print(voltage);
-  Serial.print(" sensorValue =");//ბეჭდავს შედეგს  "sensorValue ="
-  Serial.print(sensorValue);
+//  Serial.print(" sensorValue =");//ბეჭდავს შედეგს  "sensorValue ="
+//  Serial.print(sensorValue);
   Serial.println();
-  delay(300);
+//  delay(300);
+
+if(voltage>0)
+{
+
+ motorValue = map(sensorValue, 0, 1023, 0, 255);  //map ფუნქცია გვაძლევს საშუალებას ავუხსნათ ძრავს რომ 0-1023 გადანაწილდეს პროპორციულად 0-255 ზე 
+ analogWrite(motorPin, motorValue); 
+  }
+  
 
 }
-
-
-
